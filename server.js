bb521a7e7547167d6560f5caf0c03a837c068286
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-11-30 10:51:14 +0700

    add dadtabase env

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host:  process.env.POSTGRES_HOST,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
-    },
+        connectionString: process.env.POSTGRES_URL,
+        ssl:true
+
+    }
 });
 
 app.use(bodyParser.json())
